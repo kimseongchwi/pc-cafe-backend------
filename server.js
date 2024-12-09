1610f4e46c89b718d60203d27900286086300030
kimseongchwi <kimsung01265@gmail.com>
2024-12-09 23:56:46 +0900

    업로드 명칭 변경

--- server.js
+++ server.js
@@ -23,14 +23,17 @@ if (!fs.existsSync(uploadsDir)) {
 
 app.use('/uploads', express.static('uploads'));
 
+// 파일 저장 설정
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/'); // 파일이 저장될 경로
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+    filename: (req, file, cb) => {
+      // 파일 이름을 UTF-8로 변환
+      const originalName = Buffer.from(file.originalname, 'latin1').toString('utf8');
+      cb(null, originalName);
     }
-});
+  });
 
 const upload = multer({ storage: storage });
 
